1e21e6a2e4f7de557998a6147cb908d17a6e5dec
Alessandro Cabral Dev jr <101727054+Cabraless@users.noreply.github.com>
2022-03-21 14:52:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let musicas = [
-  {titulo:'Cacife Clandestino', artista:'Felp22', source:'musicas\y2meta.com - Felp 22 - Melhor Momento (Álbum Completo) (128 kbps) (1).mp3', img:'imagens/2cdf433f606eb589948229dabd194e7f.jpg'},
-  {titulo:'Hip', artista:'Classicas', source:'musicas/y2meta.com - BEST HIPHOP MIX - 50 Cent, Method Man, Ice Cube , Snoop Dogg , The Game and more (128 kbps) (1).mp3', img:'imagens/73d5f976fd4ed8bafcff08ae6ad4bd7a.jpg'}
- 
-];
+  {titulo:'Cacife Clandestino', artista:'Felp22', source:'musicas/y2meta.com - Felp 22 - Melhor Momento (Álbum Completo) (128 kbps) (1).mp3', img:'imagens/2cdf433f606eb589948229dabd194e7f.jpg'},
+  {titulo:'Hip', artista:'Classicas', source:'musicas/y2meta.com - BEST HIPHOP MIX - 50 Cent, Method Man, Ice Cube , Snoop Dogg , The Game and more (128 kbps) (1).mp3', img:'imagens/73d5f976fd4ed8bafcff08ae6ad4bd7a.jpg'},
+  {titulo:'Peaky', artista:'Blinder', source:'musicas/y2meta.com - Songs that make you feel like Thomas Shelby (128 kbps).mp3', img:'imagens/e7d9f4c5130889ade6300a0522c0f9d2.jpg'},
+ ];
 
 // INICIO
 let musica = document.querySelector('audio');
@@ -29,7 +29,7 @@ musica.addEventListener('timeupdate', atualizarBarra);
 document.querySelector('.anterior').addEventListener('click', () => {
   musicaIndex--; 
   if (musicaIndex < 0){
-      musicaIndex = 2;
+      musicaIndex = 3;
   }
   renderizarMusica(musicaIndex);
 });
